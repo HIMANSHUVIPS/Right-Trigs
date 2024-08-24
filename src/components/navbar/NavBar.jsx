39e7a1446a08a1990dd40622d5c1c3c308a4a8b9
Himanshu Sharma <sharmahimanshuvdi@gmail.com>
2024-08-24 14:15:27 +0530

    navbar completed

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./NavBar.module.css";
+const NavBar = ({logo}) => {
+  return (
+    <nav>
+      <div className={styles.logo}>
+        <div className={styles.img}>
+          <img src={logo} alt="" srcset="" />
+        </div>
+        <div className={styles.logo_name}>
+          <h1>RIGHT TRIGS</h1>
+          <h3>MENTAL HEALTH</h3>
+        </div>
+      </div>
+      <div className={styles.nav_content}>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Get Your Personalised Video Now!</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
