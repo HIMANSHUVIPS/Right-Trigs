eb810d01dd156ed89e7af513a6d7586432a7b093
Himanshu Sharma <sharmahimanshuvdi@gmail.com>
2024-08-24 18:32:15 +0530

    heading page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./App.module.css";
 import logo from "./assets/logo.webp";
+import img from "./assets/pexels-maksgelatin-4348589.jpg";
 import NavBar from "./components/navbar/NavBar";
+import Heading from "./components/Heading/Heading";
 const App = () => {
   return (
     <div className={styles.container}>
       <NavBar logo={logo}/>
+      <Heading img={img}/>
     </div>
   );
 };
---
+++ src/components/Heading/Heading.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from "./Heading.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+const Heading = ({img}) => {
+  return (
+    <div className={styles.container}>
+       <div className={styles.img}>
+        <img src={img} alt="" srcset="" />
+        <div className={styles.text}>
+        <h1>Take a free mental health test</h1>
+        <p>Our short online mental health screening tests will help you determine if you should seek help from a licensed mental health professional to address mental health issues.</p>
+        <FontAwesomeIcon icon={faChevronDown} className={styles.downArrow}/>
+       </div>
+       </div>
+       
+    </div>
+  )
+}
+
+export default Heading
