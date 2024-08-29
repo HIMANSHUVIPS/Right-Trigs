d591c7428cb3c37f59d2406e6e22f2cb41c3816b
Himanshu Sharma <sharmahimanshuvdi@gmail.com>
2024-08-29 19:52:23 +0530

    create TestDetail object and appebd the data in TestDetail page

---
+++ src/Data/TestDetailData.js
@@ -0,0 +1,42 @@
+import hands from "../assets/hands.jpg";
+import yoga from "../assets/yoga.jpg";
+import meditation from "../assets/meditation.jpg";
+import depression from "../assets/Depression.jpg";
+export const TestDetailData = {
+    "Anxiety": {
+        id: 1,
+        name: "Anxiety Disorder Test",
+        discription: "Take our short online anxiety screening test, and learn whether you may be living with excessive anxiety.",
+        img: hands,
+    },
+    "Depression": {
+        id: 2,
+        name: "Depression Test",
+        discription: "Wondering if it’s just a phase or something more? Take our short online depression test to find out if you’re living with depression.",
+        img: depression,
+    },
+    "Bipolar Disorder": {
+        id: 3,
+        name: "Bipolar Disorder Test",
+        discription: "Is it just a phase or something more? Take our short online bipolar disorder test to find out if you're living with bipolar disorder.",
+        img:yoga,
+    },
+    "OCD": {
+        id: 4,
+        name: "Obsessive Compulsive Disorder (OCD) Test",
+        discription: "Is it just a phase or something more? Take our short online OCD test to find out if you're living with obsessive compulsive disorder (OCD).",
+        img: meditation,
+    },
+    "Panic Disorder": {
+        id: 5,
+        name: "Panic Disorder Test",
+        discription: "Is it just a phase or something more? Take our short online panic disorder test to find out if you're living with panic disorder.",
+        img:yoga,
+    },
+    "Insomnia": {
+        id: 6,
+        name: "Insomnia Test",
+        discription: "Is it just a messed up sleep schedule or something more keeping you awake? Take our short online sleep insomnia test to find out if you're living with the sleep disorder insomnia.",
+        img: depression,
+    },
+};
--- src/components/TestDetailPage/TestDetail.jsx
+++ src/components/TestDetailPage/TestDetail.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./TestDetail.module.css";
+import { TestDetailData } from "../../Data/TestDetailData";
 
 const TestDetail = () => {
-    const {name} = useParams();
+  const { name } = useParams();
+  const test = TestDetailData[name];
+  if (!test) {
+    return (
+      <>
+        <h2>Test not found</h2>
+        {console.log(name)}
+      </>
+    );
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+}, []);
+
+
   return (
+    <div className={styles.container}>
+      <div className={styles.firstSection}>
+        <div className={styles.leftContent}>
+          <h1>{test.name}</h1>
+          <h4>{test.discription}</h4>
+          <button type="button">Get Started</button>
+        </div>
+        <div className={styles.rightContent}>
+          <img src={test.img} alt=""/>
+        </div>
+      </div>
+      <div className={styles.secondSection}></div>
+    </div>
+  );
+};
 
-    <div>{name} this is my test</div>
-  )
-}
-
-export default TestDetail
+export default TestDetail;
