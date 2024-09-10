ea4fe2bd7145f39bc6d5b7cdbf673289749709e7
Himanshu Sharma <sharmahimanshuvdi@gmail.com>
2024-09-10 08:55:50 +0530

    filter links items animations done

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,6 +4,27 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import gsap from "gsap";
 const Search = ({ handleFilterClick}) => {
+  useGSAP(()=>{
+    gsap.registerPlugin(ScrollTrigger);
+    
+    gsap.from('#links', {
+      opacity: 0,
+      x: 120,
+      scale: 0.8,
+      rotation: 10, 
+      duration: 1.5,
+      ease: "power2.out",
+      stagger: 0.2, 
+      scrollTrigger: {
+        trigger: '#links',
+        markers: true, 
+        scrub: true,
+        start: 'top 85%', 
+        end: 'top 50%', 
+        toggleActions: "play none none reverse", 
+      }
+    });
+  })
   const items = ["All", "Health", "IQ", "Career", "Relationships"];
   const [activeIndex, setActiveIndex] = useState(0);
   const handleClick = (index, item) => {
@@ -16,6 +37,7 @@ const Search = ({ handleFilterClick}) => {
       {items.map((item, index) => (
         <a
           key={index}
+          id="links"
           onClick={() => handleClick(index, item)}
           className={index === activeIndex ? styles.active : ""}
         >
